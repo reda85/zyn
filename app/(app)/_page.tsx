e92174c491b26347cb555f6a78d581b193d12a34
reda85 <redamarouane@gmail.com>
2026-01-21 22:08:35 +0100

    please

--- app/(app)/_page.tsx
+++ app/(app)/_page.tsx
@@ -1,15 +1,11 @@
 import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
+import { useUserData } from "@/hooks/useUserData";
 import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage() {
-  const supabase = await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
+ const {user} = useUserData();
   if (!user) {
     return redirect("/sign-in");
   }
--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,12 +2,10 @@ import { createClient } from '../../utils/supabase/server';
 import { redirect } from 'next/navigation';
 
 import { UserProviderWrapper } from '../../components/UserPoviderWrapper';
+import { useUserData } from '@/hooks/useUserData';
 
 export default async function ProtectedLayout({ children }: { children: React.ReactNode }) {
-  const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  const {user} = useUserData();
 
   console.log('user', user)
 
